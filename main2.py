c24f255d0e2dc57488e81e4f61ddf62b09d77761
Eduar-SC <https://github.com/Eduar-SC>
2025-06-13 22:03:04 -0500

    tercer intento gaaa

--- main2.py
+++ main2.py
@@ -8,23 +8,27 @@ import shutil
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-
-# # Cargar modelo entrenado
-# model = tf.keras.models.load_model('brain_tumor_cnn.h5')
-
 # Diccionario de clases
 class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
-    # Cargar modelo TFLite
+# Cargar modelo entrenado pero de h5
+# model = tf.keras.models.load_model('brain_tumor_cnn.h5')
+
+# Carpeta de subida
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# ------------------------------
+# Cargar modelo TFLite
 def load_tflite_model():
-    model_path =  "brain_tumor_cnn.tflite"
-    interpreter = tf.lite.Interpreter(model_path=model_path)
+    model_path = 'brain_tumor_cnn.tflite'
+    interpreter =tf.lite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
     return interpreter
 
 tflite_interpreter = load_tflite_model()
-
-# Función para predecir con TFLite
+#-------------------------------
+# Función para predecir usando el modelo TFLite
 def predict_image_class(interpreter, img_array):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
@@ -32,25 +36,19 @@ def predict_image_class(interpreter, img_array):
     input_shape = input_details[0]['shape']
     input_index = input_details[0]['index']
     output_index = output_details[0]['index']
-
-    # Asegurar que la imagen tenga el tipo correcto
+    # Asegurarse de que la imagen tenga el tamaño correcto
     img_array = img_array.astype(np.float32)
 
     # Cargar el tensor de entrada
     interpreter.set_tensor(input_index, img_array)
-
     # Ejecutar la inferencia
     interpreter.invoke()
 
-    # Obtener el resultado
+    # Obtener la predicción
     output_data = interpreter.get_tensor(output_index)
     return output_data[0]
+#-------------------------------
 
-
-# Carpeta de subida
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -77,7 +75,7 @@ def index():
             img_array = img_array / 255.0
 
             # Predicción
-            prediction =predict_image_class(tflite_interpreter, img_array)
+            prediction = predict_image_class(tflite_interpreter,img_array)
             predicted_class = class_names[np.argmax(prediction)]
             prediction_result = f"Predicción: {predicted_class.upper()}"
             probabilities = {class_names[i]: float(f"{prob:.4f}") for i, prob in enumerate(prediction)}
@@ -95,4 +93,5 @@ def index():
     return render_template('index.html', prediction=prediction_result, image_name=filename, probs=probabilities)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
